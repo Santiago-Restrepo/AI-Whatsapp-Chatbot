cf97deba539ababe590a0b277a9fb3d980d7ac07
restreposanti <santirstrp@gmail.com>
2024-03-28 22:29:34 -0500

    feat: first data extraction

--- services/webscraper.py
+++ services/webscraper.py
@@ -1,4 +1,6 @@
-from playwright.sync_api import sync_playwright, Page
+from playwright.sync_api import sync_playwright, Page, Browser
+import requests
+import json
 
 
 class Scraper:
@@ -6,7 +8,11 @@ class Scraper:
         self.base_url = "https://www.personeriamedellin.gov.co"
 
     def run(self):
-        self.extract()
+        extracted_data = self.extract()
+
+        with open('data/data.json', 'w') as f:
+            json.dump(extracted_data, f)
+
         # transform()
         # load()
 
@@ -19,23 +25,48 @@ class Scraper:
             business_information = self.get_business_information(page)
             business_history = self.get_business_history(page)
             business_ethics = self.get_business_ethics(page)
-            # business_objectives = self.get_business_objectives(page)
-            business_services = self.get_business_services()
-            business_fqa = self.get_business_fqa()
+            business_objectives = self.get_business_objectives(page)
+            business_services = self.get_business_services(browser)
+            business_fqa = self.get_business_fqa(page)
 
             browser.close()
 
-        return business_information, business_history, business_ethics, business_objectives, business_services, business_fqa
+        return {
+            'business_information': business_information,
+            'business_history': business_history,
+            'business_ethics': business_ethics,
+            'business_objectives': business_objectives,
+            'business_services': business_services,
+            'business_fqa': business_fqa
+        }
 
     def get_business_information(self, page: Page):
         path = '/nuestra-historia-anterior/mision-vision-valores/'
         page.goto(self.base_url + path)
         elements = page.query_selector_all('.infobox-content p')
-        scraped_data = [
-            element.inner_text()
-            for element in elements
-        ]
-        return scraped_data
+        mission_element = elements[0]
+        vision_element = elements[1]
+        mission = mission_element.inner_text()
+        vision = vision_element.inner_text()
+        elements = page.query_selector_all('.infobox-content')
+        elements = elements[2:]
+        values = []
+        for element in elements:
+            title = element.query_selector('.title').inner_text()
+            description_elements = element.query_selector_all('p')
+            description = ''
+            for description_element in description_elements:
+                description += description_element.inner_text()
+            values.append({
+                'title': title,
+                'description': description
+            })
+
+        return {
+            'mision': mission,
+            'vision': vision,
+            'values': values
+        }
 
     def get_business_history(self, page: Page):
         path = '/nuestra-historia/'
@@ -70,14 +101,61 @@ class Scraper:
             })
         return scraped_data
 
-    def get_business_objectives(self):
+    def get_business_objectives(self, page: Page):
         pass
 
-    def get_business_services(self):
-        pass
+    def get_business_services(self, browser: Browser):
+        # page = browser.new_page()
+        url = 'https://personeriaenlinea.personeriamedellin.gov.co/Servicio/GetServicio/'
+        paths = [
+            'Tutela',
+            'ImpugnacionTutela',
+            'DerechoPeticion',
+            'Asesoria',
+            'IncidenteDesacato',
+            'IncidenteDesacato',
+            'ReclamacionDerechosConsumidor',
+            'ReposicionVictimasConflictoArmado',
+            'PQRSRD'
+        ]
 
-    def get_business_fqa(self):
-        pass
+        services = []
+
+        for path in [paths[0]]:
+            response = requests.get(url + path)
+            response_json = response.json()
+            name = response_json['data']['nombre']
+            description = response_json['data']['descripcion']
+            response_time = response_json['data']['tiempoMaximoSolucion']
+
+            services.append({
+                'name': name,
+                'description': description,
+                'response_time': response_time
+            })
+        return services
+
+    def get_business_fqa(self, page: Page):
+        path = '/preguntas-frecuentes/'
+        page.goto(self.base_url + path)
+        containers = page.query_selector_all(
+            '.eael-accordion-list')
+        faqs = []
+
+        for container in containers:
+            question_container = container.query_selector(
+                '.elementor-tab-title')
+            question = question_container.inner_text()
+            response_container = container.query_selector(
+                '.eael-accordion-content')
+            response = response_container.inner_text()
+
+            faqs.append({
+                'question': question,
+                'response': response
+            })
+
+        return faqs
 
     def transform():
         pass
