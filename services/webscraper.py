b8259a26d0dbcbe548654ce480f9a31506f63108
restreposanti <santirstrp@gmail.com>
2024-03-30 20:07:54 -0500

    feat: data transformation

--- services/webscraper.py
+++ services/webscraper.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright, Page, Browser
+from bs4 import BeautifulSoup
 import requests
 import json
 
@@ -8,12 +9,15 @@ class Scraper:
         self.base_url = "https://www.personeriamedellin.gov.co"
 
     def run(self):
-        extracted_data = self.extract()
 
-        with open('data/data.json', 'w') as f:
+        extracted_data = self.extract()
+        with open('data/extracted.json', 'w') as f:
             json.dump(extracted_data, f)
 
-        # transform()
+        transformed_data = self.transform(extracted_data)
+        print(len(transformed_data))
+        with open('data/transformed.json', 'w') as f:
+            json.dump(transformed_data, f)
         # load()
 
     def extract(self):
@@ -25,8 +29,7 @@ class Scraper:
             business_information = self.get_business_information(page)
             business_history = self.get_business_history(page)
             business_ethics = self.get_business_ethics(page)
-            business_objectives = self.get_business_objectives(page)
-            business_services = self.get_business_services(browser)
+            business_services = self.get_business_services()
             business_fqa = self.get_business_fqa(page)
 
             browser.close()
@@ -35,7 +38,6 @@ class Scraper:
             'business_information': business_information,
             'business_history': business_history,
             'business_ethics': business_ethics,
-            'business_objectives': business_objectives,
             'business_services': business_services,
             'business_fqa': business_fqa
         }
@@ -71,6 +73,7 @@ class Scraper:
     def get_business_history(self, page: Page):
         path = '/nuestra-historia/'
         page.goto(self.base_url + path)
+        page.screenshot
         elements = page.query_selector_all('.twae-content')
         scraped_data = []
         for element in elements:
@@ -101,10 +104,7 @@ class Scraper:
             })
         return scraped_data
 
-    def get_business_objectives(self, page: Page):
-        pass
-
-    def get_business_services(self, browser: Browser):
+    def get_business_services(self):
         # page = browser.new_page()
         url = 'https://personeriaenlinea.personeriamedellin.gov.co/Servicio/GetServicio/'
         paths = [
@@ -121,7 +121,7 @@ class Scraper:
 
         services = []
 
-        for path in [paths[0]]:
+        for path in paths:
             response = requests.get(url + path)
             response_json = response.json()
             name = response_json['data']['nombre']
@@ -157,8 +157,148 @@ class Scraper:
 
         return faqs
 
-    def transform():
-        pass
+    def transform_business_information(self, business_information):
+        prompts = []
+        mission_prompt = {
+            'prompt': '¿Cuál es la misión de la Personería de Medellín?',
+            'response': business_information['mision']
+        }
+        vision_prompt = {
+            'prompt': '¿Cuál es la visión de la Personería de Medellín?',
+            'response': business_information['vision']
+        }
+        values_response = [
+            value['title']
+            for value in business_information['values']
+        ]
+        values_prompt = {
+            'prompt': '¿Cuales son los valores de la Personería de Medellín?',
+            'response': ', '.join(values_response)
+        }
+        prompts.append(mission_prompt)
+        prompts.append(vision_prompt)
+        prompts.append(values_prompt)
+        for value in business_information['values']:
+            prompt = {
+                'prompt': '¿Cómo gestiona la Personería de Medellín su valor de {}?'.format(value['title']),
+                'response': value['description']
+            }
+            prompts.append(prompt)
+        return prompts
+
+    def transform_business_history(self, business_history):
+        prompts = []
+        prompt = {
+            'prompt': '¿Cual es la historia de la Personería de Medellín?',
+            'response': '\n'.join(['En {}, {}'.format(item['date'], item['description']) for item in business_history])
+        }
+        prompts.append(prompt)
+        prompt = {
+            'prompt': 'Cuáles fueron las épocas de la historia de la Personería de Medellín?',
+            'response': '\n'.join([item['date'] for item in business_history])
+        }
+        prompts.append(prompt)
+        prompt = {
+            'prompt': '¿Cual es la evolución de la Personería de Medellín?',
+            'response': '\n'.join(['En {}, {}'.format(item['date'], item['description']) for item in business_history])
+        }
+        prompts.append(prompt)
+        for item in business_history:
+            prompt = {
+                'prompt': '¿Qué sucedió en {}, en el contexto de la Personería de Medellín?'.format(item['date']),
+                'response': item['description']
+            }
+            prompts.append(prompt)
+        return prompts
+
+    def transform_business_ethics(self, business_ethics):
+        prompts = []
+        questions = [
+            '¿Cuál es el código ético de la personería de medellín {}',
+            '¿Cómo se comporta la Personería de Medellín {}',
+            '¿Cuál es el código de ética de la Personería de Medellín {}',]
+        for question in questions:
+            for item in business_ethics:
+                prompt = {
+                    'prompt': question.format(item['topic']),
+                    'response': '\n'.join(item['items']).strip()
+                }
+                prompts.append(prompt)
+        return prompts
+
+    def transform_business_services(self, business_services):
+        prompts = []
+        questions = [
+            '¿Cual es el tiempo de respuesta para solicitar {}?',
+            '¿Cuánto se demoran en responder a la solicitud {}?',
+        ]
+        for service in business_services:
+            name = service['name']
+            for question in questions:
+                prompt = {
+                    'prompt': question.format(name),
+                    'response': service['response_time']
+                }
+                prompts.append(prompt)
+
+        questions = [
+            '¿Qué servicios ofrece la Personería de Medellín?',
+            '¿Cuáles son los servicios que ofrece la Personería de Medellín?',
+            'Servicios'
+        ]
+        for question in questions:
+            prompt = {
+                'prompt': question,
+                'response': ','.join([item['name'] for item in business_services])
+            }
+            prompts.append(prompt)
+        questions = [
+            '¿Cómo gestiona la Personería de Medellín el servicio de {}?',
+            '¿Quiero saber más sobre el servicio de {}?'
+        ]
+        for service in business_services:
+            name = service['name']
+            html_description = service['description']
+            soup = BeautifulSoup(html_description, 'html.parser')
+            description = soup.get_text()
+            for question in questions:
+                prompt = {
+                    'prompt': question.format(name),
+                    'response': description
+                }
+                prompts.append(prompt)
+
+        return prompts
+
+    def transform_business_fqa(self, business_fqa):
+        prompts = [
+            {
+                'prompt': item['question'],
+                'response': item['response']
+            }
+            for item in business_fqa
+        ]
+        return prompts
+
+    def transform(self, scraped_data):
+        transformed_business_data = self.transform_business_information(
+            scraped_data['business_information'])
+        transformed_business_history = self.transform_business_history(
+            scraped_data['business_history'])
+
+        transformed_business_ethics = self.transform_business_ethics(
+            scraped_data['business_ethics'])
+
+        transformed_business_services = self.transform_business_services(
+            scraped_data['business_services'])
+
+        transformed_business_fqa = self.transform_business_fqa(
+            scraped_data['business_fqa'])
+
+        prompts = transformed_business_data + transformed_business_history + \
+            transformed_business_ethics + transformed_business_services + transformed_business_fqa
+
+        return prompts
 
     def load():
         pass
