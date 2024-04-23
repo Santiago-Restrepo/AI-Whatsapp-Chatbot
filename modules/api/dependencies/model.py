40543519d9e83b13ec54709974bcdc9bcc63ce71
JuanHerrera8 <herrera.francojd@gmail.com>
2024-04-22 20:16:45 -0500

    feat: activate llm

--- modules/api/dependencies/model.py
+++ modules/api/dependencies/model.py
@@ -2,8 +2,9 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     pipeline,
+    BitsAndBytesConfig,
 )
-
+import torch 
 
 class Model:
 
@@ -17,8 +18,35 @@ class Model:
         self.tokenizer = AutoTokenizer.from_pretrained(
             model_name, trust_remote_code=True, )
         
+
+        use_4bit = True
+        bnb_4bit_compute_dtype = "float16"
+        bnb_4bit_quant_type = "nf4"
+        use_nested_quant = False
+
+        device_map = {"": 0}
+
+
+        compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+
+        bnb_config = BitsAndBytesConfig(
+            load_in_4bit=use_4bit,
+            bnb_4bit_quant_type=bnb_4bit_quant_type,
+            bnb_4bit_compute_dtype=compute_dtype,
+            bnb_4bit_use_double_quant=use_nested_quant,
+        )
+
+        if compute_dtype == torch.float16 and use_4bit:
+            major, _ = torch.cuda.get_device_capability()
+            if major >= 8:
+                print("=" * 80)
+                print("Your GPU supports bfloat16: accelerate training with bf16=True")
+                print("=" * 80)
+
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name,
+            quantization_config=bnb_config,
+            device_map=device_map
         )
 
     def predict(self, prompt):
--- modules/api/services/llm.py
+++ modules/api/services/llm.py
@@ -1,6 +1,6 @@
 from dependencies.model import Model
 
-model = Model("santi-restrepo-poli/Llama-2-7b-chat-hf", True)
+model = Model("santi-restrepo-poli/Llama-2-7b-chat-hf")
 
 def generate_llm_response(**kwargs):
     user_message = kwargs['webhook_data']['message']
