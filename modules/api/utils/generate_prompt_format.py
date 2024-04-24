dc60c163be0f9bb97ea169d708b0cc49780f5c3d
restreposanti <santirstrp@gmail.com>
2024-04-23 21:04:40 -0500

    fix: context generation

--- modules/api/utils/generate_prompt_format.py
+++ modules/api/utils/generate_prompt_format.py
@@ -3,6 +3,8 @@ from models.message import Message
 def generate_prompt_format(messages: list[Message]):
   formatted_messages = []
   for msg in messages:
-      formatted_msg = f'<s>[INST] {msg["message"]} {msg["response"]}[/INST]</s>'
+      formatted_msg = f'<s>[INST] {msg["body"]} {msg["response"]}[/INST]</s>'
       formatted_messages.append(formatted_msg)
+
+  formatted_messages = '\n'.join(formatted_messages)
   return formatted_messages
